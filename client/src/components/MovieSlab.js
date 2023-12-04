683e662a2e66f765edce0865a5d7a9e777b0571e
erubae <eruba.e@outlook.com>
2023-12-04 12:56:23 -0500

    Update MovieSlab.js

--- client/src/components/MovieSlab.js
+++ client/src/components/MovieSlab.js
@@ -10,7 +10,7 @@ const MovieSlab = ({movie}) => {
       width={100}
       height={100}
       style={{
-        width: '10%',
+        width: '100%',
         height: 'auto',
       }} src={movie.image}></Image>
     </Link>
