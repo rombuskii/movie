49699f32846273bba7c445cb7acb49ccf8dbbd48
erubae <eruba.e@outlook.com>
2023-12-04 13:05:19 -0500

    Update MovieSlab.js

--- client/src/components/MovieSlab.js
+++ client/src/components/MovieSlab.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const MovieSlab = ({movie}) => {
   return (
-    <Link style={{ position: 'relative', width: '20rem', height: '10rem' }} className='mx-2 xl:mx-10' href={`/show/${movie.id}`}>
+    <Link style={{ position: 'relative', width: '100px', height: '150px' }} className='mx-2 xl:mx-10' href={`/show/${movie.id}`}>
       <Image className='rounded-md duration-300 hover:scale-110' sizes="100px"
       fill
       style={{
